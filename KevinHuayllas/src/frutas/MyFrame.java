d3926e79618ba152eb100705e924d33db7bc4576
Kevin Huayllas <KevinOmega01@gmail.com>
2024-04-08 16:57:46 -0400

    agregando un nuevo Frame

---
+++ KevinHuayllas/src/frutas/MyFrame.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package frutas;
+
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author TUF
+ */
+public class MyFrame {
+    MyFrame(){
+        JFrame frame = new JFrame(); 
+        frame.setTitle("JFrame title goes here"); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+//        frame.setResizable(false); 
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+//        frame.setSize(1920,1080); 
+        frame.setVisible(true); 
+  
+        ImageIcon image = new ImageIcon("logo.png"); 
+        frame.setIconImage(image.getImage()); 
+        frame.getContentPane().setBackground(new Color(0xf9f8fa)); 
+    }
+}
